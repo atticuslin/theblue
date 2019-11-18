a1142128223656c59a30a2f19b22fe94ae63c0f6
Atticus <atticus.lin@gmail.com>
2019-11-18 11:10:12 -0500

    Initial skeleton

---
+++ app.py
@@ -0,0 +1,29 @@
+#LSPT Search Engine, Team theblue
+
+crawl_links = []
+
+
+'''
+Returns true if url exists in graph
+'''
+def exists(url):
+	pass
+
+'''
+Insert link into webgraph and return success or fail
+'''
+def update(url):
+	pass
+
+'''
+Get next links, post to crawling, and remove from queue after confirmation
+TODO: POST to crawling?
+'''
+def getNextCrawl(max):
+	pass
+
+'''
+l is list of URLs from ranking; return dictionary/JSON with URL -> PageRank Value
+'''
+def rank(l):
+	pass
