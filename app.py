198f29be223ff7203c8fa4acd4521a14ada68f7b
Atticus <atticus.lin@gmail.com>
2019-11-21 10:42:50 -0500

    Javadoc comments

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 #LSPT Search Engine, Team theblue
 
 crawl_links = []
+graph = {} #Will be created in neo4j
 
 
 '''
 Returns true if url exists in graph
+@param url URL whose existance this function checks
+@return True if URL is in graph
 '''
 def exists(url):
 	pass
 
 '''
 Insert link into webgraph and return success or fail
+@param url URL to be inserted/updated in graph
+@modify Graph by adding outlinks from url to corresponding nodes in graph
 '''
 def update(url):
 	pass
 
 '''
 Get next links, post to crawling, and remove from queue after confirmation
-TODO: POST to crawling?
+@param max Maximum number of links to crawl
+@return Returns list of max number of links to be crawled
 '''
 def get_next_crawl(max):
 	pass
@@ -25,6 +31,8 @@ def get_next_crawl(max):
 '''
 l is list of URLs from ranking; return dictionary/JSON with URL -> PageRank Value
 POST sent to /rank_list
+@param l list of links to rank in order
+@return list of links ranked by pagerank
 '''
 def rank(l):
 	pass
