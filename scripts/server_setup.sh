89f2b208a1f97b62ba421e5405cbe605e6a4ec99
atticuslin <atticus.lin@gmail.com>
2019-12-05 10:32:26 -0500

    Setup scripts

--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -3,5 +3,9 @@ sudo pip3 install flask
 sudo pip3 install flask_api
 sudo pip3 install requests
 sudo pip3 install gunicorn
+sudo pip3 install neobolt
+sudo apt-get install 
+sudo pip3 install neo4j
 git checkout master
 git pull origin master
+sudo curl -sSL https://get.docker.com/ | sh
